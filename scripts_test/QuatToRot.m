1e8114b8662ac2772c07ef9a0aa245831bc89da0
JBannwarth <jban039@aucklanduni.ac.nz>
2017-10-02 18:14:06 +1300

    Fix steady state error when subject to wind disturbance.
    The integral error would get reset each time because of the is_rotary_wing variable in the attitude controller was set to false instead of true.

--- scripts_test/QuatToRot.m
+++ scripts_test/QuatToRot.m
@@ -1,22 +1,6 @@
 function R = QuatToRot( q )
 % QUATERNIONTOROTATIONMATRIXMAT Convert quaternion to rotation matrix
 % Written by: J.X.J. Bannwarth, 27/03/17
-
-%     aSq = q(1) * q(1);
-%     bSq = q(2) * q(2);
-%     cSq = q(3) * q(3);
-%     dSq = q(4) * q(4);
-%     
-%     R(1,1) = aSq + bSq - cSq - dSq;
-%     R(1,2) = 2.0 * (q(2) * q(3) - q(1) * q(4));
-%     R(1,3) = 2.0 * (q(1) * q(3) + q(2) * q(4));
-%     R(2,1) = 2.0 * (q(2) * q(3) + q(1) * q(4));
-%     R(2,2) = aSq - bSq + cSq - dSq;
-%     R(2,3) = 2.0 * (q(3) * q(4) - q(1) * q(2));
-%     R(3,1) = 2.0 * (q(2) * q(4) - q(1) * q(3));
-%     R(3,2) = 2.0 * (q(1) * q(2) + q(3) * q(4));
-%     R(3,3) = aSq - bSq - cSq + dSq;
-
     W = q(1);
     X = q(2);
     Y = q(3);
