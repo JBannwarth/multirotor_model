1784260c426154f3a1b101014d4f54427781b16f
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2018-08-03 10:50:57 +1200

    Fix issues, switch to NED

--- scripts_simulation/LineariseOctocopter2D.m
+++ scripts_simulation/LineariseOctocopter2D.m
@@ -5,8 +5,8 @@ Uav.D_UAV = 0.456;
 Uav.D_PROP = 10 * 0.0254;
 Uav.A_UAV = (pi*Uav.D_UAV^2)/4;
 Uav.A_PROP = (pi*Uav.D_PROP^2)/4;
-UAV.ZETA = deg2rad(31);
+Uav.ZETA = deg2rad(31);
 Uav.N_ROTORS = 8;
 Uav.C_T = 0.5*0.005052618460001*Uav.RHO_AIR*Uav.D_PROP^2*Uav.A_PROP;
-
+Initial.OMEGA = ones(8,1)*500;
 
