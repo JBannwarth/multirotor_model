74a437d76b31348f807c8a92e4d188a6e830a817
JBannwarth <jban039@aucklanduni.ac.nz>
2019-01-31 16:59:57 +1300

    Add new motor map with selectable airframe configuration

---
+++ matlab_systems/MotorMap.m
@@ -0,0 +1,101 @@
+classdef MotorMap < matlab.System & matlab.system.mixin.CustomIcon & ...
+        matlab.system.mixin.SampleTime & matlab.system.mixin.Propagates
+    %MOTORMAP Convert from PX4 motor ordering to NED CCW ordering
+    %   See PX4 airframe reference (retrieved 2019/01/31, v1.82):
+    %       https://dev.px4.io/en/airframes/airframe_reference.html
+    %   Written:       J.X.J. Bannwarth, 2019/01/31
+    %   Last modified: J.X.J. Bannwarth, 2019/01/31
+
+    properties (Nontunable)
+        airframeConfig(1,1) {mustBeGreaterThanOrEqual(airframeConfig, 1), mustBeInteger(airframeConfig)} = 5; % Airframe configuration
+    end
+
+    properties(Access = private)
+        rotorOrder = -1;
+    end
+
+    properties(Constant,Access = private)
+        airframeTypes = struct( 'HexarotorP', [1 4 6 2 3 5], ...
+            'HexarotorX', [5 1 4 6 2 3], ...
+            'OctorotorP', [1 3 8 4 2 6 7 5], ...
+            'OctorotorX', [1 3 8 4 2 6 7 5], ...
+            'QuadrotorP', [3 1 4 2], ...
+            'QuadrotorH', [1 4 2 3], ...
+            'QuadrotorWide', [1 4 2 3], ...
+            'QuadrotorX', [1 4 2 3], ...
+            'TricopterYP', [1 3 2], ...
+            'TricopterYM', [1 3 2] );
+    end
+    
+    methods
+        function set.airframeConfig( obj, val )
+            % Support name-value pair arguments when constructing object
+            if val > numel(fieldnames(obj.airframeTypes))
+                error( 'Value must be less than or equal to %d.', ...
+                    numel(fieldnames(obj.airframeTypes)) );
+            end
+            obj.airframeConfig = val;
+        end
+    end
+
+    methods(Access = protected)
+        function setupImpl( obj )
+            % Perform one-time calculations, such as computing constants
+            airframes = fieldnames(MotorMap.airframeTypes);
+            obj.rotorOrder = obj.airframeTypes.(airframes{obj.airframeConfig});
+        end
+
+        function validateInputsImpl( obj, px4PWM )
+            % Validate inputs to the step method at initialization
+            airframes = fieldnames(MotorMap.airframeTypes);
+            rotors = obj.airframeTypes.(airframes{obj.airframeConfig});
+            if length(px4PWM) ~= length(rotors)
+                error( 'Block input should have a length of %d.', ...
+                    length(rotors) )
+            end
+        end
+
+        function flag = isInputSizeMutableImpl( ~, ~ )
+            % Return false if input size cannot change
+            % between calls to the System object
+            flag = false;
+        end
+
+        function simPWM = stepImpl( obj, px4PWM )
+            simPWM = px4PWM( obj.rotorOrder );
+        end
+
+        function icon = getIconImpl( obj )
+            % Define icon for System block
+            airframes = fieldnames(MotorMap.airframeTypes);
+            lastLine = replace( airframes{obj.airframeConfig}, 'P', '+' );
+            lastLine = replace( lastLine, 'M', '-' );
+            icon = [ "MOTOR", "MAP", lastLine ];
+        end
+
+        function out = getOutputSizeImpl( obj )
+            % Inherit size from first input port
+            out = propagatedInputSize(obj,1);
+        end
+        
+    end
+
+    methods(Access = protected, Static)
+        function header = getHeaderImpl
+            % Define header panel for System block dialog
+            definition = [ ...
+                'Convert from PX4 motor ordering to NED CCW ordering\n' ...
+                'Available frames:\n' ] ;
+            airframes = fieldnames(MotorMap.airframeTypes);
+            for i = 1:numel( airframes )
+                airframes{i} = replace( airframes{i}, 'P', '+' );
+                airframes{i} = replace( airframes{i}, 'M', '-' );
+                definition = [ definition, sprintf( '%02d %s ', i, airframes{i} ), '\n' ];
+            end
+            definition = sprintf( definition(1:end-2) );
+            header = matlab.system.display.Header( 'MotorMap', ...
+                'Title', 'Motor Map', ...
+                'Text', definition );
+        end
+    end
+end
