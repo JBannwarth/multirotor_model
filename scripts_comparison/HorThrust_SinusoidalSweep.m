73754d07f9340579f163a7be8664e38d8cb41ff6
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2020-04-26 20:12:07 +1200

    Add support for FSFUfullGain, switch to .mat v7.3, set freq in Hz

--- scripts_comparison/HorThrust_SinusoidalSweep.m
+++ scripts_comparison/HorThrust_SinusoidalSweep.m
@@ -1,16 +1,19 @@
 %HORTHRUST_SINUSOIDALSWEEP Run sine sweep experiment
 %   Written by:    J.X.J. Bannwarth, 2018/05/16
-%	Last modified: J.X.J. Bannwarth, 2019/05/16
+%	Last modified: J.X.J. Bannwarth, 2020/04/23
 
 %% First initialization
 close all; clc; clearvars;
-ctrlName = 'FPHT';
+ctrlName = 'MIS';
 project = simulinkproject; projectRoot = project.RootFolder;
 
 %% Generate output folder
 tTmp = clock;
 outputFile = sprintf( 'SinusoidalSweep_%s_%d-%02d-%02d_%02d-%02d-%02.f', ctrlName, tTmp(1:6));
 outputFolder = fullfile( projectRoot, 'data_results', 'sine_sweep' );
+if ~exist(outputFolder, 'dir')
+   mkdir(outputFolder)
+end
 
 %% Load wind data
 ULin = [4 0 0]; % About halfway through the wind tunnel range
@@ -27,7 +30,9 @@ switch ctrlName
     case 'IHT'
         InitializePx4v1_8IHT
     case 'FPHT'
-        InitializePx4v1_8FPHT
+        InitializePx4v1_8FPHT    
+    case 'FPHTFullGain'
+        InitializePx4v1_8FPHTFullGain
     case 'FPHTSimple'
         InitializePx4v1_8FPHTTranslationOnly
     case 'MIS'
@@ -41,7 +46,7 @@ set_param( [model '/Sinusoidal input'], 'Commented', 'off' )
 Wind.Ampl = [0.5;0;0];
 Wind.Bias = ULin;
 Wind.Freq = [1;0;0];
-freqs = logspace(-1,1,10);
+freqs = 2*pi*logspace(-1,2,50);
 periods = 2*pi./freqs;
 tEnd = 4*periods;
 tEnd( periods < 10 ) = tEnd( periods<10) + 10;
@@ -58,4 +63,4 @@ end
 simOut = sim( simIn );
 
 %% Save data
-save( fullfile( outputFolder, outputFile ), 'simIn', 'simOut', 'ULin' )
+save( fullfile( outputFolder, outputFile ), 'simIn', 'simOut', 'ULin', '-v7.3' )
