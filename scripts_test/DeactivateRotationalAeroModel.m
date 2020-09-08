fb1da142330af6c7a2a2874aa70ac4a8de142b71
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2020-09-08 14:04:55 +1200

    Add script to deactivate rotational aerodynamic model

---
+++ scripts_test/DeactivateRotationalAeroModel.m
@@ -0,0 +1,7 @@
+%DEACTIVATEROTATIONALAEROMODEL Deactive rotational effects
+%   Written by:    J.X.J. Bannwarth, 2020/09/04
+Aero.CM1.coefs = Aero.CM1.coefs.*0;
+Aero.CM2.coefs = Aero.CM2.coefs.*0;
+Aero.Cz1.coefs = Aero.Cz1.coefs.*0;
+Aero.Cz3.coefs = Aero.Cz3.coefs.*0;
+Aero.Cx2.coefs = Aero.Cx2.coefs.*0;
