161e8fb2383f2f5ab41d92bfeba7984336f4fe07
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2020-12-17 09:05:59 +1300

    PHD-150 Adjust operating point variables - WIP
    MATLAB can't find operating point yet, need to debug this

--- scripts_init/LinearizeUAVAtt.m
+++ scripts_init/LinearizeUAVAtt.m
@@ -1,6 +1,5 @@
 %LINEARIZEUAVATT Linearize UAV model with attitude controller
 %   Written by:    J.X.J. Bannwarth, 2019/05/02
-%   Last modified: J.X.J. Bannwarth, 2019/05/02
 
 clearvars;
 project = simulinkproject; projectRoot = project.RootFolder;
@@ -33,7 +32,8 @@ set_param( [model '/Motor model'], 'ModelName', 'MotorModelZJChen' );
 set_param( model, 'LoadInitialState', 'off' );
 set_param( model, 'LoadExternalInput', 'off' );
 
-% Comment out unnecessary blocks
+% Use input blocks
+set_param( [model '/Input choice'], 'Value', '4' ) % Wind input
 switches = find_system( model, 'SearchDepth', '1', 'regexp', 'on', ...
         'IncludeCommented', 'on', 'Name', 'Manual Switch \w');
 constants = find_system( model, 'SearchDepth', '1', 'regexp', 'on', ...
@@ -42,7 +42,7 @@ for ii = 1:length( switches )
     set_param( switches{ii}, 'sw', '0' )
 end
 for ii = 1:length(constants)
-    set_param( constants{ii}, 'Commented', 'on' )
+    set_param( constants{ii}, 'Commented', 'off' )
 end
     
 % Load UAV parameters
@@ -91,10 +91,10 @@ end
 inputSpecs = { ...
                'U'               , 'u'    , ULin         ;
                'U'               , 'Known', [ 1 1 1 ]'   ;
-               'virtualThrustDes', 'Known', [ 0 1 0 ]'   ;
-               'yawDes'          , 'Known', 0;
+               'virtualThrustDes', 'Known', [ 0 0 0 ]'   ;
+               'yawDes'          , 'Known', 1;
                'yawRateDes'      , 'Known', 1            ;
-               'horThrust'       , 'Known', [ 1 1 ]'     ;
+               'horThrustDes'    , 'Known', [ 1 1 ]'     ;
                };
 
 for i = 1:size(inputSpecs,1)
@@ -116,18 +116,30 @@ op = findop( model, opspec, options );
 set_param( model, 'LoadInitialState', 'on' );
 set_param( model, 'InitialState', 'getstatestruct(op)' );
 
+% Write equilibrium points to constant blocks for manual validation
+for ii = 1:length(constants)
+    var = strsplit( constants{ii}, ' ' );
+    var = char( join( var(2:end) ) );
+    
+    % getInputIndex returns multiple indexes but only the first one is
+    % necessary
+    idxs = getInputIndex( op, [model '/' var] );
+    vals = op.Inputs( idxs(1,1) ).u;
+    set_param( constants{ii}, 'Value', mat2str( vals ) )
+end
+
 %% Linearize model
 % Get linear model
 inputs = { ...
-    'U'         , 1;
-    'etaDes'    , 1;
-    'horThrust' , 1;
-    'thrustDes' , 1;
-    'yawRateDes', 1;
+    'U'               , 1;
+    'virtualThrustDes', 1;
+    'horThrustDes'    , 1;
+    'yawDes'          , 1;
+    'yawRateDes'      , 1;
     };
 
 outputs = { ...
-    % 'Quadortor Euler Model', 8; % eta
+%     'xiInt'                , 1; % xiInt
     'Quadrotor Euler Model', 5; % xi
     'Quadrotor Euler Model', 6; % xidot
     };
@@ -141,4 +153,4 @@ end
 
 linsys = linearize( model, io, op );
 
-save( fullfile( projectRoot, 'work', 'Octocopter_LinMod_Att'), 'linsys', 'op' )
+% save( fullfile( projectRoot, 'work', 'Octocopter_LinMod_Att'), 'linsys', 'op' )
