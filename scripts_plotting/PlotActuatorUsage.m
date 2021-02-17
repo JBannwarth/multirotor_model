986464211dfec8fe196ab9a3a41b78e2e9ebc3f6
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-02-17 17:42:14 +1300

    PHD-150 Enable for plotting multiple result files

--- scripts_plotting/PlotActuatorUsage.m
+++ scripts_plotting/PlotActuatorUsage.m
@@ -1,9 +1,17 @@
 %PLOTACTUATORUSAGE Compute and plot actuator usage metrics
-%   Written by: J.X.J. Bannwarth, 2020/12/09
+%   Input:
+%       - resultFile: string or cell array of result files to analyse. If
+%                     not defined, use most recent file.
+%       - windFile:   if the result files contain result for several wind
+%                     files, selects which to use.
+%
+%   See also HORTHRUST_POSHOLD.
+%
+%   Written: 2020/12/09, J.X.J. Bannwarth
 
 %% Clean-up and set-up
 clearvars -except resultFile windFile
-clc; close all;
+close all
 project = simulinkproject; projectRoot = project.RootFolder;
 
 %% Input processing
@@ -12,53 +20,67 @@ windFolder = fullfile( projectRoot, 'data_wind', 'TurbSimOC' );
 
 if ~exist( 'resultFile', 'var' )
     % Select latest file
-    allResultFiles = dir(resultFolder);
-    allResultFiles = {allResultFiles.name};
-    % Naming convention is chronological when sorted in ASCII order
-    resultFile = allResultFiles{end};
+    resultFile = GetLatestFile( resultFolder, '*.mat' );
 end
 
 if ~exist( 'windFile', 'var' )
+    % Select highest wind speed
     windFile = 'TurbSim_40_01.mat';
 end
 
-%% Load data and select desired response
-load( fullfile( resultFolder, resultFile ), 'ULin', 'simIn', 'simOut'  )
-
-windFiles = simIn.getVariable( 'windFile' );
-idx = find( ismember( windFiles, windFile ) );
-if isempty( idx )
-    error( [ 'The result file does not contain results for the desired ' ...
-        'speed' ] )
+if ~iscell( resultFile )
+    % To prevent errors when calling resultFile{ii}
+    resultFile = {resultFile};
 end
 
-simOut = simOut(idx);
-simIn = simIn(idx);
-ULin = ULin(idx, :);
+% Maximum filename length
+maxLen = max( cellfun( @(s)(length(s)), resultFile ) );
 
-%% Process data
-logsout = simOut.get('logsout');
-pwmData = logsout.get('realPwm').Values;
-t = pwmData.Time;
-pwm = pwmData.Data;
+%% Process results
+for ii = 1:length( resultFile )
+    %% Load data and select desired response
+    load( fullfile( resultFolder, resultFile{ii} ), 'ULin', 'simIn', 'simOut'  )
+    fprintf( sprintf('[%% %ds] ', maxLen), resultFile{ii} )
 
-pwmStd = std( pwm, 1 );
+    windFiles = simIn.getVariable( 'windFile' );
+    idx = find( ismember( windFiles, windFile ) );
+    if isempty( idx )
+        error( [ 'The result file does not contain results for the desired ' ...
+            'speed' ] )
+    end
 
-fprintf( 'Standard dev. PWM values (us):' )
-for ii = 1:length(pwmStd)
-    fprintf( ' [%d] %.2f,', ii, pwmStd(ii) )
+    simOut = simOut(idx);
+    simIn = simIn(idx);
+    ULin = ULin(idx, :);
+
+    %% Process data
+    logsout = simOut.get('logsout');
+    pwmData = logsout.get('realPwm').Values;
+    t = pwmData.Time;
+    pwm = pwmData.Data;
+
+    pwmStd = std( pwm, 1 );
+
+    % Print standard deviations
+    fprintf( 'PWM std (us):' )
+    for jj = 1:length(pwmStd)
+        fprintf( ' [%d]% 6.2f,', jj, pwmStd(jj) )
+    end
+    fprintf( ' [mean]% 6.2f, [min]% 6.2f, [max]% 6.2f\n', mean( pwmStd ), ...
+        min(pwmStd), max(pwmStd) )
+
+    %% Plot data
+    figSize = [15 15];
+    fontSize = 12;
+
+    figure( 'name', sprintf( 'PWM signals - %s', resultFile{ii} ) )
+    plot( t, pwm )
+    
+    % Format
+    xlabel( 'Time (s)' )
+    ylabel( 'PWM ($\mu$s)' )
+    legend( compose( 'Rotor %d', 1:size(pwm, 2) ), 'location', 'best' )
+    title( sprintf( '$\\bar{\\sigma}_\\mathrm{PWM} = %.2f \\,\\mu$s', ...
+        mean( pwmStd ) ) )
+    SetFigProp( figSize, fontSize )
 end
-fprintf( ' [mean] %.2f, [min] %.2f, [max] %.2f\n', mean( pwmStd ), ...
-    min(pwmStd), max(pwmStd) )
-
-%% Plot data
-figSize = [15 15];
-fontSize = 12;
-
-figure( 'name', 'PWM signals' )
-plot( t, pwm )
-xlabel( 'Time (s)' )
-ylabel( 'PWM ($\mu$s)' )
-legend( compose( 'Rotor %d', 1:size(pwm, 2) ), 'location', 'best' )
-title( sprintf( '$\\bar{\\sigma}_\\mathrm{PWM} = %.2f \\,\\mu$s', mean( pwmStd ) ) )
-SetFigProp( figSize, fontSize )
