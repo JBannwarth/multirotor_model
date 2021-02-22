9d97724bb3b279c969e5db4b7d34312ea7730fa9
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2021-02-22 15:59:52 +1300

    PHD-157 Convert to function

--- scripts_test/DeactivateRotationalAeroModel.m
+++ scripts_test/DeactivateRotationalAeroModel.m
@@ -1,7 +1,11 @@
+function Aero = DeactivateRotationalAeroModel( Aero )
 %DEACTIVATEROTATIONALAEROMODEL Deactive rotational effects
+%   AERO = DEACTIVATEROTATIONALAEROMODEL( AERO )
+%
 %   Written by:    J.X.J. Bannwarth, 2020/09/04
-Aero.CM1.coefs = Aero.CM1.coefs.*0;
-Aero.CM2.coefs = Aero.CM2.coefs.*0;
-Aero.Cz1.coefs = Aero.Cz1.coefs.*0;
-Aero.Cz3.coefs = Aero.Cz3.coefs.*0;
-Aero.Cx2.coefs = Aero.Cx2.coefs.*0;
+    Aero.CM1.coefs = Aero.CM1.coefs.*0;
+    Aero.CM2.coefs = Aero.CM2.coefs.*0;
+    Aero.Cz1.coefs = Aero.Cz1.coefs.*0;
+    Aero.Cz3.coefs = Aero.Cz3.coefs.*0;
+    Aero.Cx2.coefs = Aero.Cx2.coefs.*0;
+end
