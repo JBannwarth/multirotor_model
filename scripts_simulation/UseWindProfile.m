2a39559edf8115d9625461c7d47a74caa51cf3ec
Jérémie Bannwarth <jban039@aucklanduni.ac.nz>
2021-03-08 22:15:37 +1300

    PHD-158 Change to work with new signal controlled switch

--- scripts_simulation/UseWindProfile.m
+++ scripts_simulation/UseWindProfile.m
@@ -1,12 +1,12 @@
 function UseWindProfile( modelName, toUse )
 %USEWINDPROFILE Switches the varying wind input on/off
-%   Written by: Jeremie Bannwarth, 21/08/2017
+%   Written: 21/08/2017, Jeremie Bannwarth
     
     if toUse
+        set_param( [modelName '/Input choice'], 'Value', '1' );
         set_param( [modelName '/Varying wind input'], 'commented', 'off' );
-        set_param( [modelName '/Wind switch'], 'sw', '1' );
     else
+        set_param( [modelName '/Input choice'], 'Value', '2' );
         set_param( [modelName '/Varying wind input'], 'commented', 'on' );
-        set_param( [modelName '/Wind switch'], 'sw', '0' );
     end
 end
