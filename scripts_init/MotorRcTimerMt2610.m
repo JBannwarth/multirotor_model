a527ff5d7ccc2a6a3f858d5cc5b4287f5b75a829
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2021-04-28 19:00:13 +1200

    PHD-169 Update motor parameters to match thesis model

--- scripts_init/MotorRcTimerMt2610.m
+++ scripts_init/MotorRcTimerMt2610.m
@@ -14,38 +14,25 @@ function Motor = MotorRcTimerMt2610( nRotors )
 
     %% MOTOR ELECTRICAL PROPERTIES
     % RCTimer MT2610, 920KV - modelled as brushed DC motor
-    Motor.R   = 0.11;              % motor resistance [Ohm]
-    Motor.K_E = 1 / (920*pi/30);   % motor back emf Constant [Vs/rad]
-    Motor.K_T = Motor.K_E * (3/2); % effective motor torque constant [Nm/A] (3/2 factor for BLDC c.f. brushed DC)
+    % Datasheet properties
+    Motor.R   = 0.11;         % Resistance [ohm]
+    Motor.V_0 = 11.1;         % Nominal voltage [V]
+    Motor.I_0 = 0.7;          % Idle current [A]
+    Motor.K_V = 2*pi*920/60;  % Speed constant [rad/sV]
+    
+    % Measured values
+    Motor.C_TAU = 4.4335282346e-7; % Aerodynamic torque coefficient [Nms^2/rad^2]
+    
+    % Calculated values
+    Motor.K = 7.59588e-6; % Thrust coefficient [Ns^2/rad^2], 960g thrust @ 11.1V, 15.2A
+    Motor.K_E = (Motor.V_0-Motor.I_0*Motor.R) / (Motor.K_V*Motor.V_0); % Back EMF constant [Nms/rad]
+    Motor.K_T = Motor.K_E; % Motor torque constant [Nm/A]
 
-    % Rotor thrust coefficient
-    % 960g thrust @ 11.1V, 15.2A
-    Motor.K = 7.59588e-6;
-
-    % Rotor drag coefficient
-    Motor.B = 4.4335282346e-7 .* ones(1, nRotors);
-
-    % Masses
-    Motor.M_M = 0.054; % Motor
-    Motor.M_P = 0.011; % Propeller
-
-    % Motor inertia [kgm^2]
-    Motor.R_M = 0.016;
-    Motor.H_M = 0.0193;
-    Motor.I_M = Motor.M_M * diag( [(1/12) * (3*Motor.R_M^2 + Motor.H_M^2), ...
-                                   (1/12) * (3*Motor.R_M^2 + Motor.H_M^2), ...
-                                   (1/2)  * Motor.R_M^2] );
-
-    % Propeller inertia (only model inertia in z-axis) [kgm^s]
-    Motor.R_P = 10 * 0.0254 / 2; % 13*0.0254/2;
-    Motor.I_P = Motor.M_P*diag([0, 0, (1/12) * (2*Motor.R_P)^2]);
-
-    % Rotor inertia (spinning part of motor + propeller) [kgm^s]
-    Motor.M_R = 0;
-    Motor.R_R = 0;
-    Motor.H_R = 0;
+    %% MOTOR MECHANICAL PROPERTIES
+    mProp = 0.011; % [kg]
+    dProp = 10 * 0.0254; % [m]
     for i = 1:nRotors
-        Motor.I_R(:,:,i) = (Motor.I_M + Motor.I_P);
+        Motor.I_R(:,:,i) = mProp * diag( [0, 0, (1/12) * dProp^2 ] );
     end
     Motor.I_R_ZZ = reshape( Motor.I_R(3,3,:), [1, size(Motor.I_R,3)]);
 end
