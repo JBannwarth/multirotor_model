43237469a3988851463b837094d8600f36558a6e
Jeremie Bannwarth <jban039@aucklanduni.ac.nz>
2021-04-30 11:25:13 +1200

    PHD-16 Add script to convert SS model to C++ Px4-matrix matrices

---
+++ scripts_synthesis/ExportController.m
@@ -0,0 +1,44 @@
+function out = ExportController( filename, dt )
+%EXPORTCONTROLLER Export state space controller to C++ Px4-Matrix format
+%   OUT = EXPORTCONTROLLER( ) exports 'work/HinfGain.mat'
+%   OUT = EXPORTCONTROLLER( FILENAME ) exports filename
+%   OUT = EXPORTCONTROLLER( FILENAME, DT ) specifies the sampling rate
+%
+%   See also CREATEDOFHINFCONTROLLER.
+%
+%   Written: 2021/04/29, J.X.J. Bannwarth
+    arguments
+        filename (1,:) char   = fullfile('work', 'HinfGain.mat')
+        dt       (1,1) double = 0.02 % 50 Hz
+    end
+    
+    %% Load file
+    % Add root folder so that this can be called from anywhere as long as
+    % the project is open
+    project = simulinkproject;
+    filename = fullfile( project.RootFolder, filename );
+    load( filename, 'K', 'thrustOp' )
+    
+    %% Prepare controller
+    % Discretise the controller
+    Kd = c2d( K, dt, 'Tustin' );
+    A = Kd.A; B = Kd.B; C = Kd.C; D = Kd.D;
+    m = size(A,1);
+    n = size(B,2);
+    p = size(C,1);
+    
+    
+    %% Export controller
+    out = sprintf( ['static constexpr uint8_t _ctrl_num_inputs{%d};\n' ...
+                    'static constexpr uint8_t _ctrl_num_outputs{%d};\n' ...
+                    'static constexpr uint8_t _ctrl_num_states{%d};\n' ], ...
+                    n, p, m );
+    out = [ out ...
+            sprintf('float _data_A[%d] {', m*m) sprintf('%.8ff,', A(:)' ) sprintf('};\n') ...
+            sprintf('float _data_B[%d] {', m*n) sprintf('%.8ff,', B(:)' ) sprintf('};\n') ...
+            sprintf('float _data_C[%d] {', p*m) sprintf('%.8ff,', C(:)' ) sprintf('};\n') ...
+            sprintf('float _data_D[%d] {', p*n) sprintf('%.8ff,', D(:)' ) sprintf('};\n') ...
+            sprintf('float _data_op[%d] {', p)  sprintf('%.8ff,', thrustOp(:)' ) sprintf('};\n')];
+    out = replace( out, ',}', '}' );
+    out = replace( out, {'-0.00000000','0.00000000'}, '0.0' );        
+end
